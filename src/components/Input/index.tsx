654f6f7a13d6b6ca0f816e7bc94bfcc142f911d1
William <william.jose.wjd@gmail.com>
2020-10-12 20:29:12 -0300

    terminando primeira parte da estilizaçao do SignIn

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,7 +11,11 @@ interface InputProps extends TextInputProps{
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
   <Container>
     <Icon name={icon} size={20} color="#666360" />
-    <TextInput keyboardAppearance="dark" placeholderTextColor="#666360" {...rest} />
+    <TextInput
+      keyboardAppearance="dark"
+      placeholderTextColor="#666360"
+      {...rest}
+    />
   </Container>
 );
 
